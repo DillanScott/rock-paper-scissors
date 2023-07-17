ca4bc1cedbbe787f3880fd65682f3d2e5cd6bb56
DillanScott <90711877+DillanScott@users.noreply.github.com>
2023-07-17 23:33:27 +0100

    Implement getComputerChoice() and playRound()
    The first to return a random choice of rock, paper or scissors for the
    computer's choice
    And the second to take the player and computer input and return a string
    stating who won the round

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-console.log("test")
+function getComputerChoice() {
+    let randomNumber = Math.floor(Math.random() * 3)
+    console.log(randomNumber)
+    switch (randomNumber) {
+        case 0:
+            return "ROCK"
+        case 1:
+            return "PAPER"
+        case 2:
+            return "SCISSORS"
+    }
+}
+
+function playRound(playerChoice, computerChoice) {
+    playerChoice = playerChoice.toUpperCase()
+    computerChoice = computerChoice.toUpperCase()
+
+    if (playerChoice == computerChoice) {
+        return "It's a tie! " + playerChoice + " vs " + computerChoice
+    } else if ((computerChoice === 'ROCK' && playerChoice === 'SCISSORS') ||
+               (computerChoice === 'SCISSORS' && playerChoice === 'PAPER') ||
+               (computerChoice === 'PAPER' && playerChoice === 'ROCK')) {
+        return "You lose! " + computerChoice + " beats " + playerChoice
+    } else if ((playerChoice === 'ROCK' && computerChoice === 'SCISSORS') ||
+               (playerChoice === 'SCISSORS' && computerChoice === 'PAPER') ||
+               (playerChoice === 'PAPER' && computerChoice === 'ROCK')) {
+        return "You win! " + playerChoice + " beats " + computerChoice
+}
+}
