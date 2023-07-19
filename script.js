6284d0a951a1723542ab9e7c0bb971b49dfecb9b
DillanScott <90711877+DillanScott@users.noreply.github.com>
2023-07-19 18:28:33 +0100

    Implement working rps game, with buttons etc.
    TODOs:
    - Add game over pop up and play again button to index.html
    - Add scripting for these in script.js

--- script.js
+++ script.js
@@ -1,62 +1,121 @@
-function getComputerChoice() {
-    let randomNumber = Math.floor(Math.random() * 3)
-    console.log(randomNumber)
-    switch (randomNumber) {
-        case 0:
-            return "ROCK"
-        case 1:
-            return "PAPER"
-        case 2:
-            return "SCISSORS"
-    }
-}
+// GAME LOGIC
+
+let playerScore = 0;
+let computerScore = 0;
+let roundWinner = "";
 
 function playRound(playerChoice, computerChoice) {
-    playerChoice = playerChoice.toUpperCase()
-    computerChoice = computerChoice.toUpperCase()
-
+    console.log(playerChoice + "  " + computerChoice);
     if (playerChoice === computerChoice) {
-        return "TIE"
+        roundWinner = "TIE";
     } else if ((computerChoice === 'ROCK' && playerChoice === 'SCISSORS') ||
-               (computerChoice === 'SCISSORS' && playerChoice === 'PAPER') ||
-               (computerChoice === 'PAPER' && playerChoice === 'ROCK')) {
-        return "LOSS"
+        (computerChoice === 'SCISSORS' && playerChoice === 'PAPER') ||
+        (computerChoice === 'PAPER' && playerChoice === 'ROCK')) {
+        computerScore++;
+        roundWinner = "COMPUTER";
     } else if ((playerChoice === 'ROCK' && computerChoice === 'SCISSORS') ||
-               (playerChoice === 'SCISSORS' && computerChoice === 'PAPER') ||
-               (playerChoice === 'PAPER' && computerChoice === 'ROCK')) {
-        return "WIN"
-}
+        (playerChoice === 'SCISSORS' && computerChoice === 'PAPER') ||
+        (playerChoice === 'PAPER' && computerChoice === 'ROCK')) {
+        playerScore++;
+        roundWinner = "PLAYER";
+    }
+    updateScoreboard(roundWinner, playerChoice, computerChoice);
 }
 
-function game() {
-    let playerScore = 0
-    let computerScore = 0
-    for (let i = 0; i < 5; i++) {
-        let playerChoice = prompt("Choose: Rock, Paper or Scissors").toUpperCase()
-        let computerChoice = getComputerChoice()
-        let outcome = playRound(playerChoice, computerChoice)
-        
-        switch (outcome) {
-            case "WIN":
-                playerScore++
-                console.log("You win! " + playerChoice + " beats " + computerChoice)
-                break
-            case "LOSS":
-                computerScore++
-                console.log("You lose! " + computerChoice + " beats " + playerChoice)
-                break
-            case "TIE":
-                console.log("It's a tie! " + playerChoice + " vs " + computerChoice)
-        }
-        console.log("Player: " + playerScore + "   Computer: " + computerScore)    
-    }
-    if (playerScore > computerScore) {
-        console.log("YOU WON! The score was " + playerScore + " - " + computerScore)
-    } else if (playerScore < computerScore) {
-        console.log("YOU LOST! The score was " + playerScore + " - " + computerScore)
-    } else {
-        console.log("YOU TIED! The final score was " + playerScore + " - " + computerScore)
+function getComputerChoice() {
+    let randomNumber = Math.floor(Math.random() * 3);
+    // console.log(randomNumber);
+    switch (randomNumber) {
+        case 0:
+            return "ROCK";
+        case 1:
+            return "PAPER";
+        case 2:
+            return "SCISSORS";
     }
 }
 
-game()
+function isGameOver() {
+    return playerScore === 5 || computerScore === 5;
+}
+
+// UI
+
+const outcome = document.getElementById("outcome");
+const outcomeInfo = document.getElementById("outcomeInfo");
+const playerScoreDisp = document.getElementById("playerScore");
+const computerScoreDisp = document.getElementById("computerScore");
+const playerChoiceDisp = document.getElementById("playerChoice");
+const computerChoiceDisp = document.getElementById("computerChoice");
+
+const rockBtn = document.getElementById("rockBtn");
+const paperBtn = document.getElementById("paperBtn");
+const scissorsBtn = document.getElementById("scissorsBtn");
+
+// TODO: Engame stuff
+
+rockBtn.addEventListener("click", () => handleClick("ROCK"));
+paperBtn.addEventListener("click", () => handleClick("PAPER"));
+scissorsBtn.addEventListener("click", () => handleClick("SCISSORS"));
+
+
+function handleClick(playerChoice) {
+    if (isGameOver()) {
+        // openEndgameModal();
+        console.log("GAME OVER!");
+        return;
+    }
+
+    let computerChoice = getComputerChoice();
+    playRound(playerChoice, computerChoice);
+    updateChoice(playerChoiceDisp, playerChoice);
+    updateChoice(computerChoiceDisp, computerChoice);
+    updateScore();
+
+    if (isGameOver()) {
+        console.log("GAME OVER!");
+        // openEndgameModal();
+        // setFinalMessage();
+    }
+}
+
+function updateChoice(playerDisp, choice) {
+    switch (choice) {
+        case "ROCK":
+            playerDisp.textContent = "✊";
+            break;
+        case "PAPER":
+            playerDisp.textContent = "✋";
+            break;
+        case "SCISSORS":
+            playerDisp.textContent = "✌";
+    }
+}
+
+function updateScore() {
+    if (roundWinner === 'TIE') {
+        outcome.textContent = "It's a tie!";
+    } else if (roundWinner === 'PLAYER') {
+        outcome.textContent = 'You won!';
+    } else if (roundWinner === 'COMPUTER') {
+        outcome.textContent = 'You lost!';
+    }
+
+    playerScoreDisp.textContent = `Player: ${playerScore}`;
+    computerScoreDisp.textContent = `Computer: ${computerScore}`;
+}
+
+function updateScoreboard(winner, playerChoice, computerChoice) {
+    if (winner === 'PLAYER') {
+        outcomeInfo.textContent = `${playerChoice} beats ${computerChoice}`;
+        return;
+    }
+    if (winner === 'COMPUTER') {
+        outcomeInfo.textContent = `${playerChoice} is beaten by 
+            ${computerChoice}`;
+        return;
+    }
+
+    outcomeInfo.textContent = `${playerChoice} ties with 
+        ${ computerChoice}`;
+}
