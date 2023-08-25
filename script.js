121a6407cb01e305208da3dde893561ddfb49c42
DillanScott <90711877+DillanScott@users.noreply.github.com>
2023-08-25 18:39:45 +0100

    Implement working end game pop up

--- script.js
+++ script.js
@@ -52,16 +52,19 @@ const rockBtn = document.getElementById("rockBtn");
 const paperBtn = document.getElementById("paperBtn");
 const scissorsBtn = document.getElementById("scissorsBtn");
 
-// TODO: Engame stuff
+const endGameWindow = document.getElementById("endGameWindow");
+const playAgainBtn = document.getElementById("playAgainBtn");
+const finalResult = document.getElementById("finalResult");
 
 rockBtn.addEventListener("click", () => handleClick("ROCK"));
 paperBtn.addEventListener("click", () => handleClick("PAPER"));
 scissorsBtn.addEventListener("click", () => handleClick("SCISSORS"));
+playAgainBtn.addEventListener("click", () => location.reload());
 
 
 function handleClick(playerChoice) {
     if (isGameOver()) {
-        // openEndgameModal();
+        showEndGameWindow();
         console.log("GAME OVER!");
         return;
     }
@@ -74,8 +77,7 @@ function handleClick(playerChoice) {
 
     if (isGameOver()) {
         console.log("GAME OVER!");
-        // openEndgameModal();
-        // setFinalMessage();
+        showEndGameWindow();
     }
 }
 
@@ -118,4 +120,14 @@ function updateScoreboard(winner, playerChoice, computerChoice) {
 
     outcomeInfo.textContent = `${playerChoice} ties with 
         ${ computerChoice}`;
+}
+
+function showEndGameWindow() {
+    if (playerScore > computerScore) {
+        finalResult.textContent = `You won. The score was ${playerScore} - ${computerScore}`;
+    }
+    else if (playerScore < computerScore) {
+        finalResult.textContent = `You lost. The score was ${playerScore} - ${computerScore}`;
+    }
+    endGameWindow.style.visibility = "visible";
 }
